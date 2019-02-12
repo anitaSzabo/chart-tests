043981c4faceea238828116897a49db1499fbedf
anitaSzabo <azorka3@gmail.com>
2019-02-12 17:21:42 +0100

    details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
+import { MatCardModule} from '@angular/material';
+import { MatSlideToggleModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule} from '@angular/material';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyBarChartComponent
+    MyBarChartComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    ChartsModule
+    ChartsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DetailsComponent]
 })
+
 export class AppModule { }
--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { MatDialog, MatDialogConfig} from '@angular/material';
+import {DetailsComponent} from '../details/details.component';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-my-bar-chart',
@@ -9,24 +12,15 @@ import { Chart } from 'chart.js';
 
 export class MyBarChartComponent implements OnInit {
 
-  public barChartOptions = {
-    scaleShowVerticalLines: true,
-    responsive: true
-  };
   public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'radar';
-  public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
   public chart = [];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
     this.chart = new Chart('canvas', {
       type: 'bar',
+      responsive: true,
       data: {
         labels: this.barChartLabels, // your labels array
         datasets: [
@@ -72,17 +66,64 @@ export class MyBarChartComponent implements OnInit {
       }
     });
 
-  }
+    this.chart = new Chart('canvas2', {
+      type: 'pie',
+      responsive: true,
+      data: {
+        labels: this.barChartLabels, // your labels array
+        datasets: [
+          {
+            data: [65, 59, 80, 81, 56, 55, 40], // your data array
+            borderColor: '#00AEFF',
+            backgroundColor: '#f9ff3d',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        tooltips: {
+          callbacks: {
+            label: (toolipItem, data) => {
+              var label = data.datasets[toolipItem.datasetIndex].label || '';
 
-  onClick() {
-    console.log('hahó');
+              console.log(toolipItem);
+              console.log(data);
+              console.log(label);
+            }
+          }
+        },
+        legend: {
+          display: true
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+
   }
 
   chartClicked(event: any) {
     console.log(event);
+    console.log('clicked');
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.autoFocus = true;
+    dialogConfig.height = '400px';
+    dialogConfig.width = '600px';
+    dialogConfig.closeOnNavigation = true;
+    dialogConfig.hasBackdrop = true;
+    dialogConfig.data = {
+      id: 1,
+      title: 'Details'
+    };
+
+    this.dialog.open(DetailsComponent, dialogConfig);
   }
 
-  chartHovered(event: any) {
-    console.log(event);
-  }
 }
